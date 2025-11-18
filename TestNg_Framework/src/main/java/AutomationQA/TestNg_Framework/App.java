f1c97521aa4de09ad5ffda7f947749c788bc7ddf
Lenovo <venumavooru0417@gmail.com>
2025-11-18 13:00:26 +0530

    first commit

---
+++ TestNg_Framework/src/main/java/AutomationQA/TestNg_Framework/App.java
@@ -0,0 +1,13 @@
+package AutomationQA.TestNg_Framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
